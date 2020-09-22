61555ddb725f6a4e415c2d38083416c22ce3bca4
abdulhanan5643 <59803552+abdulhanan5643@users.noreply.github.com>
2020-09-23 04:46:33 +0500

    Completed done using states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,14 +6,33 @@ import ToDoList from './components/ToDoList';
 
 function App() {
 
+  //states
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  //function
+  const filterHandler = () =>{
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+
+        case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+    
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
         <h1>ToDo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
       <ToDoList setTodos={setTodos} todos={todos} />
     </div>
   );
--- my-app/src/components/ToDo.js
+++ my-app/src/components/ToDo.js
@@ -5,17 +5,27 @@ const ToDo = ({ text, todo, todos, setTodos }) => {
     const deleteHandler = () => {
         setTodos(todos.filter(el => el.id !== todo.id));
     };
-    return (
-        <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button className="complete-btn">
-                <i className="fas fa-check"></i>
-            </button>
-            <button onClick={deleteHandler} className="trash-btn">
-                <i className="fas fa-trash"></i>
-            </button>
-        </div>
-    );
+    const completeHandler = () => {
+        setTodos(todos.map((item) =>{
+            if (item.id === todo.id) {
+                return{
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    };
+return (
+    <div className="todo">
+        <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+        <button onClick={completeHandler} className="complete-btn">
+            <i className="fas fa-check"></i>
+        </button>
+        <button onClick={deleteHandler} className="trash-btn">
+            <i className="fas fa-trash"></i>
+        </button>
+    </div>
+);
 };
 
 export default ToDo;
